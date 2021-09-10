82bf33d74a622a44f6f777025324fb7db8a75be3
Nycole <nycole.xavierr@gmail.com>
2021-09-10 11:48:25 -0300

    finish aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 60.5
+console.log(`A velocidade do seu carro é ${velocidade}`)
+if (velocidade > 60) { // condição simples
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+console.log('Diriga sempre usando cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'Brasil'
+if (pais == 'Brasil') {
+    console.log('Brasileiro')
+} else {
+    console.log('Você é estrangeiro')
+}
