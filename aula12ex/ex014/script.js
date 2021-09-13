28c4b19fe4d57a4e5d050632e02ad40c70c03a2b
Nycole <nycole.xavierr@gmail.com>
2021-09-13 08:39:16 -0300

    finish exercicios parte 2

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML =  `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA
+        img.src = 'assets/fotomanha.jpg'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18 ) {
+        // Boa tarde
+        img.src = 'assets/fototarde.jpg'
+        document.body.style.background = '#b9846f'
+    } else {
+        // noite
+        img.src = 'assets/fotonoite.jpg'
+        document.body.style.background = '#515154'
+    }
+}
