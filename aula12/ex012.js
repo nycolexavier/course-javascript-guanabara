b86929c3bcc5300f72b1a0b75d93439a18fcb9e2
Nycole <nycole.xavierr@gmail.com>
2021-09-13 07:43:59 -0300

    finish aula 12

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,5 +1,10 @@
-var hora = 8
-console.log(`Agora são exatamente %{hora} horas.`)
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
 if (hora < 12) {
     console.log('Bom dia!')
-} 
+} else if (hora <= 18) {
+    console.log('Boa tarde!')
+} else {
+    console.log('Boa noite!')
+}
