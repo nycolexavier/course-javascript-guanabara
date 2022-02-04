f25daf47097c15e017987bbcaca8cda92be110ae
Nycole Xavier <nycole.xavierr@gmail.com>
2022-02-04 11:13:39 -0400

    ➕ aula 11

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,6 +1,10 @@
-var velocidade = 60.5
-console.log(`A velocidade do seu carro é ${velocidade}`)
-if (velocidade > 60) { // condição simples
-    console.log('Você ultrapassou a velocidade permitida. Multado!')
+console.log('O console funcionou conrretamente')
+
+var vel = 60.5 // Condição simples
+console.log(`A velocidade do seu carro é ${vel} por hora.`)
+
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida, MULTADO!')
 }
-console.log('Diriga sempre usando cinto de segurança')
+
+console.log('Diriga sempre usando cinto de segurança!')
--- aula11/ex009.js
+++ aula11/ex009.js
@@ -1,6 +1,9 @@
 var pais = 'Brasil'
+
+console.log(`Vivendo em ${pais}`)
+
 if (pais == 'Brasil') {
-    console.log('Brasileiro')
+    console.log('Brasileiro!')
 } else {
-    console.log('Você é estrangeiro')
+    console.log('Você é Estrangeiro!')
 }
