0c161229173c8013e9b915d03d24dc96e7ecb4df
Nycole Xavier <nycole.xavierr@gmail.com>
2022-02-07 11:34:09 -0400

    ➕ aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+var contador = 1
+
+do {
+    console.log(`Passo ${contador}`)
+    contador++ // contador = contador + 1
+} while( contador <= 6)
+
+
+// var contador = 1
+
+// while( contador <= 6) {
+//     console.log(`Passo ${contador}`)
+//     contador++ // contador = contador + 1
+// }
+
+
+// console.log('Tudo bem!')
+// console.log('Tudo bem!')
+// console.log('Tudo bem!')
+// console.log('Tudo bem!')
+// console.log('Tudo bem!')
