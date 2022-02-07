42c80126857fad11cb9dc8a378b5e1ed97aa3020
Nycole Xavier <nycole.xavierr@gmail.com>
2022-02-07 13:51:53 -0400

    ➕ aula 14

---
+++ aula14/ambiente.js
@@ -0,0 +1,13 @@
+// var contador = 1
+
+// while (contador <= 5) {
+//     console.log(contador)
+//     contador++
+// }
+
+console.log('Vai começar...')
+for (var contador = 1; contador <= 5; contador++) {
+    console.log(contador)
+}
+
+console.log('FIM!')
