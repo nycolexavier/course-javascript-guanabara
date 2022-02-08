03b334f6dfbf85e94492c1aa283af5bdbec9100b
Nycole Xavier <nycole.xavierr@gmail.com>
2022-02-08 10:25:30 -0400

    ➕ exercício 17

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,24 @@
-alert('Olá')
+// alert('Olá')
+
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if(num.value.length == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+
+        let c = 1
+        // limpar a tabuada
+        tab.innerHTML = ''
+
+        while(c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} + ${c} = ${n*c}`
+            // item.value `tab${c}`
+            tab.appendChild(item)
+            c++;
+        }
+    }
+}
