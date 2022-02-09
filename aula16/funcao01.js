5dc6e8552975f5e18e40be977194e255d24b0bb8
Nycole Xavier <nycole.xavierr@gmail.com>
2022-02-09 09:53:47 -0400

    ➕ aula 16

---
+++ aula16/funcao01.js
@@ -0,0 +1,14 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'par'
+    } else {
+        return 'ímpar'
+    }
+} 
+
+let res = parimpar(11)
+console.log(res)
+
+// ou 
+
+console.log(parimpar(11))
